4fe971731fc0db29ba38c0cc5abc03822df2a016
satish <satishattada@gmail.com>
2024-07-10 11:34:20 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# react-static-plugin-react-router
+
+A [React-Static](https://react-static.js.org) plugin that adds support for [react-router](https://reacttraining.com/react-router/web/guides/quick-start)
+
+
+## Installation
+
+- Install this plugin and peer dependencies:
+
+```bash
+$ yarn add react-static-plugin-react-router react-router-dom react-router
+```
+
+Make sure that you have _exactly_ the same version for `react-router-dom` as `react-router`, per [this blog post](https://reacttraining.com/blog/react-router-v5/).
+
+- Add the plugin to your `static.config.js`:
+
+```javascript
+export default {
+  plugins: ['react-static-plugin-react-router'],
+}
+```
+
+- Follow the [Dynamic Routes with React Router guide](/docs/guides/dynamic-routes-react-router.md) to configure your routes for both dynamic and static rendering
+
+- (Optional) Configure the plugin:
+
+```javascript
+export default {
+  plugins: [
+    [
+      'react-static-plugin-react-router',
+      {
+        RouterProps: {
+          // These props will be passed to the underlying `Router` component
+        },
+      },
+    ],
+  ],
+}
+```
---
+++ src/browser.api.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useBasepath, useStaticInfo } from 'react-static'
+import { BrowserRouter, StaticRouter, withRouter } from 'react-router-dom'
+
+const Location = withRouter(({ children, location }) => children(location))
+
+export default ({ RouterProps: userRouterProps = {} }) => ({
+  Root: PreviousRoot => ({ children }) => {
+    let Router
+    const basepath = useBasepath()
+    const staticInfo = useStaticInfo()
+
+    const routerProps = {
+      basename: basepath, // Required
+    }
+
+    // Test for document to detect the node stage
+    if (typeof document !== 'undefined') {
+      // If in the browser, just use the browser router
+      Router = BrowserRouter
+    } else {
+      Router = StaticRouter
+      routerProps.location = staticInfo.path // Required
+      routerProps.context = {} // Required
+    }
+    return (
+      <PreviousRoot>
+        <Router {...routerProps} {...userRouterProps}>
+          {children}
+        </Router>
+      </PreviousRoot>
+    )
+  },
+  Routes: PreviousRoutes => props => (
+    <Location>
+      {location => <PreviousRoutes {...props} location={location} />}
+    </Location>
+  ),
+})
